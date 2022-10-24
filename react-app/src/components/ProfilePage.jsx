3d737f4d98e99a7871369f84935f2aa9a076a796
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-10-24 22:35:29 +0100

    added login page

---
+++ react-app/src/components/ProfilePage.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header"
+
+import './style.css'
+
+
+
+const ProfilePage =()=>{
+
+    return (
+        <>
+        <Header/>
+        <div className="profilePage">
+            <div className="sideNavBar">
+            <div className="userInfo">
+                <p>test</p>
+            </div>
+            </div>
+           
+        </div>
+        </>
+    )
+
+}
+
+export default ProfilePage
